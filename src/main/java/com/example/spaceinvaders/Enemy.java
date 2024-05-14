00850f6d6d4931dff17c3d32b27649b0aa5e676c
htl3r-2144 <2144@htl.rennweg.at>
2024-05-14 14:18:30 +0200

    Started to add code for moving enemies

--- src/main/java/com/example/spaceinvaders/Enemy.java
+++ src/main/java/com/example/spaceinvaders/Enemy.java
@@ -1,7 +1,10 @@
 package com.example.spaceinvaders;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class Enemy {
     public GameEngine gameEngine;
@@ -10,6 +13,10 @@ public class Enemy {
     public Image[] images = new Image[]{new Image("file:src/main/resources/Images/Enemy1.png"), new Image("file:src/main/resources/Images/Enemy2.png"), new Image("file:src/main/resources/Images/Enemy3.png")};
     public Image image;
     public ImageView view;
+    public int moveCounter = 0;
+    public boolean first = true;
+    public int speed = 1000;
+    public Timeline timeline = new Timeline(new KeyFrame(Duration.millis(speed), e -> updateEnemies()));
 
     public Enemy(GameEngine gameEngine, int row, int col) {
         this.gameEngine = gameEngine;
@@ -21,11 +28,43 @@ public class Enemy {
         this.view.setY(posY);
         this.view.setFitWidth(gameEngine.enemyWidth);
         this.view.setFitHeight(gameEngine.enemyHeight);
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    public void checkSpeed(){
+        switch (gameEngine.enemies.size()){
+            case 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24:
+                speed = 750;
+                break;
+            case 10, 11, 12, 13:
+                speed = 600;
+                break;
+            case 5, 6, 7, 8, 9:
+                speed = 500;
+                break;
+            case 3, 4:
+                speed = 400;
+                break;
+            case 2, 1:
+                speed = 350;
+                break;
+        }
     }
 
     public void resetImgEnemy(ImageView viewEnemy) {
         viewEnemy.setX(0);
         viewEnemy.setY(-200);
         viewEnemy.setVisible(false);
+    }
+
+    private void updateEnemies() {
+        checkSpeed();
+        if (first) {
+            for (int i = 0; i < gameEngine.enemies.size(); i++) {
+                gameEngine.enemies.get(i);
+            }
+        }
     }
 }
